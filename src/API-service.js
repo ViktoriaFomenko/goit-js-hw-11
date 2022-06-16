0a6c5a1290f8dac017b7f83acc19451d1fb05546
ViktoriaFomenko <aktavia@gmail.com>
2022-06-16 18:54:09 +0300

    render

--- src/API-service.js
+++ src/API-service.js
@@ -1,6 +1,6 @@
 export default function fetchCard(searchQuery) {
-  const url = `https://pixabay.com/api/?key=28071781-459ddb4c5fc455b50beadddbb&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
-  return fetch(url).then(response => {
+  const URL = `https://pixabay.com/api/?key=28071781-459ddb4c5fc455b50beadddbb&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`;
+  return fetch(URL).then(response => {
     return response.json();
   });
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const searchForm = document.querySelector('.search-form');
 const loadMoreBtn = document.querySelector('.load-more');
 const galleryList = document.querySelector('.gallery');
 let searchQuery = '';
-let page = 1;
 
 searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', onLoadMore);
@@ -14,19 +13,14 @@ function onSearch(e) {
   galleryList.innerHTML = '';
 
   fetchCard(searchQuery).then(renderCard);
-  // .catch(error => {
-  //   alert(
-  //     'Sorry, there are no images matching your search query. Please try again.'
-  //   );
-  // });
 }
 
 function onLoadMore() {
   fetchCard(searchQuery);
 }
 
-function renderCard(cards) {
-  const markup = cards
+function renderCard({ hits }) {
+  const markup = hits
     .map(
       ({
         webformatURL,
